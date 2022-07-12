af7e786abedd045dba68862606a009b939ea09fd
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-12 16:58:35 -0400

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { IpfsImage } from 'react-ipfs-image';
+import { useEffect, useState } from "react";
+import { IpfsImage } from "react-ipfs-image";
 
 export interface CardProps {
   name?: string;
@@ -15,12 +15,12 @@ export default function Card({
   collection,
 }: CardProps) {
   const isImage =
-    imageSlug?.split('.').pop() === 'png' ||
-    imageSlug?.split('.').pop() === 'jpg';
-  const isGif = imageSlug?.split('.').pop() === 'gif';
-  const isVideo = imageSlug?.split('.').pop() === 'mp4';
+    imageSlug?.split(".").pop() === "png" ||
+    imageSlug?.split(".").pop() === "jpg";
+  const isGif = imageSlug?.split(".").pop() === "gif";
+  const isVideo = imageSlug?.split(".").pop() === "mp4";
   const isHash = imageSlug?.length === 46;
-  const isHashUrl = imageSlug?.startsWith('ipfs://');
+  const isHashUrl = imageSlug?.startsWith("ipfs://");
 
   const [isVideoError, setIsVideoError] = useState(false);
 
@@ -34,35 +34,34 @@ export default function Card({
         <img
           src={imageSlug}
           alt={name}
-          onError={(i: any) => (i.target.style.display = 'none')}
+          onError={(i: any) => (i.target.style.display = "none")}
         />
       )}
       {isGif && (
         <img
           src={imageSlug}
           alt={name}
-          onError={(i: any) => (i.target.style.display = 'none')}
+          onError={(i: any) => (i.target.style.display = "none")}
         />
       )}
       {isVideo && <video loop autoPlay hidden muted src={imageSlug} />}
       {isHash && (
         <IpfsImage
-          hash='{imageSlug}'
-          onError={(i: any) => (i.target.style.display = 'none')}
+          hash={imageSlug as string}
+          onError={(i: any) => (i.target.style.display = "none")}
         />
       )}
       {isHashUrl && (
         <IpfsImage
-          hash='{imageSlug}'
-          onError={(i: any) => (i.target.style.display = 'none')}
+          hash={imageSlug as string}
+          onError={(i: any) => (i.target.style.display = "none")}
         />
       )}
       {!isVideo && !isGif && !isImage && (
-        <div>
           <img
             src={imageSlug}
             alt={name}
-            onError={(i: any) => (i.target.style.display = 'none')}
+            onError={(i: any) => (i.target.style.display = "none")}
           />
           {!isVideoError && (
             <video
@@ -74,15 +73,14 @@ export default function Card({
               onError={() => setIsVideoError(true)}
             />
           )}
-        </div>
       )}
-      <div className='flex flex-col space-y-2 pt-2'>
-        <a target='_blank' href={imageSlug} rel='noreferrer'>
-          <span className='font-bold mt-4 text-base sm:text-lg'>{name}</span>
+      <div className="flex flex-col space-y-2 pt-2">
+        <a target="_blank" href={imageSlug} rel="noreferrer">
+          <span className="font-bold mt-4 text-base sm:text-lg">{name}</span>
         </a>
-        <span className='text-sm sm:text-md'>{collection}</span>
-        <span className='uppercase text-xs sm:text-sm font-mono font-bold text-[#356DF3]'>
-          <span className='text-white'>Chain: </span>
+        <span className="text-sm sm:text-md">{collection}</span>
+        <span className="uppercase text-xs sm:text-sm font-mono font-bold text-[#356DF3]">
+          <span className="text-white">Chain: </span>
           {blockchain}
         </span>
       </div>
